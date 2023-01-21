ec9bddabc55dbf4c8503c236002621bfa6a73470
jamesmcewen <jamesmcewen5@gmail.com>
2023-01-21 14:01:03 -0800

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Footer from './components/layout/Footer/Footer';
+import Navigation from './components/layout/Navigation/Navigation';
 const App = () => {
   return (
     <div>
+      <Navigation />
       <Footer />
     </div>
   );
