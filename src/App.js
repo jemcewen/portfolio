5a5c42bdfbd47478a5c2d8c22347df0aaf49abd1
jamesmcewen <jamesmcewen5@gmail.com>
2023-01-25 11:58:16 -0800

    Add Projects component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navigation from './layout/Navigation/Navigation';
 import Home from './layout/Home/Home';
 import About from './layout/About/About';
+import Projects from './layout/Projects/Projects';
 import Contact from './layout/Contact/Contact';
 import Footer from './layout/Footer/Footer';
 
@@ -10,6 +11,7 @@ const App = () => {
       <Navigation />
       <Home />
       <About />
+      <Projects />
       <Contact />
       <Footer />
     </>
