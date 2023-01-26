f06ffa9e868e34536c5a0ec3f0b85e912b8490af
jamesmcewen <jamesmcewen5@gmail.com>
2023-01-26 12:52:17 -0800

    Add background overlay to mobile nav

--- src/layout/Navigation/Navigation.jsx
+++ src/layout/Navigation/Navigation.jsx
@@ -14,48 +14,56 @@ const Navigation = () => {
   };
 
   return (
-    <nav className={styles.nav}>
-      <a href='#home' className={styles.logo}>
-        James McEwen
-      </a>
-      <div>
-        <ul
-          className={
-            clicked
-              ? `${styles['nav-links']} ${styles.clicked}`
-              : `${styles['nav-links']}`
-          }
-        >
-          <li>
-            <a href='#home' onClick={closeMenu}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href='#about' onClick={closeMenu}>
-              About
-            </a>
-          </li>
-          <li>
-            <a href='#projects' onClick={closeMenu}>
-              Projects
-            </a>
-          </li>
-          <li>
-            <a href='#contact' onClick={closeMenu}>
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.burger} onClick={handleClick}>
-        {clicked ? (
-          <FaTimes className={styles.icon} />
-        ) : (
-          <FaBars className={styles.icon} />
-        )}
-      </div>
-    </nav>
+    <>
+      <div
+        className={
+          clicked ? `${styles.overlay} ${styles.show}` : `${styles.overlay}`
+        }
+      ></div>
+      <nav className={styles.nav}>
+        <a href='#home' className={styles.logo}>
+          James McEwen
+        </a>
+        <div>
+          <ul
+            className={
+              clicked
+                ? `${styles['nav-links']} ${styles.clicked}`
+                : `${styles['nav-links']}`
+            }
+          >
+            <li>
+              <a href='#home' onClick={closeMenu}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href='#about' onClick={closeMenu}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href='#projects' onClick={closeMenu}>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href='#contact' onClick={closeMenu}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.burger} onClick={handleClick}>
+          {clicked ? (
+            <FaTimes className={styles.icon} />
+          ) : (
+            <FaBars className={styles.icon} />
+          )}
+        </div>
+      </nav>
+    </>
   );
 };
 export default Navigation;
