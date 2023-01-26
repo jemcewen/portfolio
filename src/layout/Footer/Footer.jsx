b87dcece0e6bdae20640fda49bbfaf4c297aebe7
jamesmcewen <jamesmcewen5@gmail.com>
2023-01-25 16:02:51 -0800

    Add return home button

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,15 +1,29 @@
-import { FaGithubSquare, FaLinkedin, FaEnvelopeSquare } from 'react-icons/fa';
+import {
+  FaGithubSquare,
+  FaLinkedin,
+  FaEnvelopeSquare,
+  FaChevronUp,
+} from 'react-icons/fa';
 import styles from './Footer.module.css';
 
 const Footer = () => {
   const footerYear = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
+      <div className={styles['return-home']}>
+        <a href='#home'>
+          <FaChevronUp className={styles.arrow} />
+        </a>
+      </div>
       <div className={styles.socials}>
         <a href='https://github.com/jemcewen' target='_blank' rel='noreferrer'>
           <FaGithubSquare className={styles.icon} />
         </a>
-        <a href='https://www.linkedin.com/' target='_blank' rel='noreferrer'>
+        <a
+          href='https://www.linkedin.com/in/jemcewen/'
+          target='_blank'
+          rel='noreferrer'
+        >
           <FaLinkedin className={styles.icon} />
         </a>
         <a
