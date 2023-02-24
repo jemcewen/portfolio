3fc1a3d5966d4e607687e9e63b65e77f796583c0
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-23 19:08:05 -0800

    Add LinkedIn icon

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,30 +1,27 @@
-import {
-  FaGithubSquare,
-  FaLinkedin,
-  FaEnvelopeSquare,
-  FaChevronUp,
-} from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn, FaEnvelope } from 'react-icons/fa';
 import styles from './Footer.module.css';
 
 const Footer = () => {
   const footerYear = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
-      <div className={styles['return-home']}>
-        <a href='#home'>
-          <FaChevronUp className={styles.arrow} />
-        </a>
-      </div>
       <div className={styles.socials}>
         <a href='https://github.com/jemcewen' target='_blank' rel='noreferrer'>
-          <FaGithubSquare className={styles.icon} />
+          <FaGithub className={styles.icon} />
         </a>
         <a
           href='mailto:jamesmcewen5@gmail.com'
           target='_blank'
           rel='noreferrer'
         >
-          <FaEnvelopeSquare className={styles.icon} />
+          <FaEnvelope className={styles.icon} />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/jemcewen/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <FaLinkedinIn className={styles.icon} />
         </a>
       </div>
       <p className={styles.copyright}>James McEwen &copy;{footerYear}</p>
