5c543be92fe38ec04747d446876bef75d23c7880
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-28 18:50:26 -0800

    Add spacer

--- src/layout/Home/Home.jsx
+++ src/layout/Home/Home.jsx
@@ -3,14 +3,18 @@ import styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <section id='home'>
-      <div className='container'>
-        <h1 className={styles.heading}>James McEwen</h1>
-        <h3 className={styles['sub-heading']}>Full Stack Web Developer</h3>
-        <a href='#projects'>
+    <section id='home' className={styles.section}>
+      <div className={styles.content}>
+        <p className={styles.leading}>Hi, my name is</p>
+        <h1 className={styles.heading}>James McEwen.</h1>
+        <h3 className={styles['sub-heading']}>
+          I am a Full Stack Web Developer.
+        </h3>
+        <a href='#projects' className={styles.button}>
           <Button>View My Work</Button>
         </a>
       </div>
+      <div className={styles.spacer}></div>
     </section>
   );
 };
