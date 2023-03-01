7863fedd0ba162eb7672eb366fc93cb4c2c084cf
jamesmcewen <jamesmcewen5@gmail.com>
2023-02-28 18:42:51 -0800

    Move Toaster to App component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import About from './layout/About/About';
 import Projects from './layout/Projects/Projects';
 import Contact from './layout/Contact/Contact';
 import Footer from './layout/Footer/Footer';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
     <>
+      <Toaster
+        toastOptions={{
+          style: {
+            fontSize: '1.2rem',
+            borderRadius: '12px',
+            padding: '2rem',
+          },
+        }}
+      />
       <Navigation />
       <Home />
       <About />
